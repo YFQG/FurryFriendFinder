f1354251f9618004e8ba0f5a422718217a2c6631
YamidQuiroga <rechoinfomauroyid@gmail.com>
2023-05-11 16:05:56 -0500

    Add this static class to save the role for all the users

---
+++ Models/ViewModels/UserRol.cs
@@ -0,0 +1,9 @@
+﻿using FurryFriendFinder.Models.Data;
+
+namespace FurryFriendFinder.Models.ViewModels
+{
+    public static class UserRol
+    {
+        public static User User { get; set; } = new();
+    }
+}
