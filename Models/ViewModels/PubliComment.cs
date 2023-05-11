8b81c27f5be1964fb656753c3fb70536cac0947f
YamidQuiroga <rechoinfomauroyid@gmail.com>
2023-05-11 18:04:55 -0500

    Add model publi comment forthe index view of home controller

---
+++ Models/ViewModels/PubliComment.cs
@@ -0,0 +1,15 @@
+﻿using FurryFriendFinder.Models.Data;
+
+namespace FurryFriendFinder.Models.ViewModels
+{
+    public class PubliComment
+    {
+        public List<Comment> Comment = new();
+        public List<Publication> publi = new();
+        public PubliComment(List<Publication> publications, List<Comment> comments)
+        {
+            Comment = comments;
+            publi = publications;
+        }
+    }
+}
