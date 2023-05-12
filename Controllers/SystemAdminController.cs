cddaf30c6577071fe02e6a7367868fc77d37de8f
YamidQuiroga <rechoinfomauroyid@gmail.com>
2023-05-11 20:08:22 -0500

    Add system administrator controller

---
+++ Controllers/SystemAdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FurryFriendFinder.Controllers
+{
+    public class SystemAdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
