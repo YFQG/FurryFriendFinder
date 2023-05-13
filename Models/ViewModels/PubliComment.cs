51f51968c490bf57dc390c668948734f73c32866
Bryan Rojas <brayan.rojitas03@hotmail.com>
2023-05-13 17:38:04 -0500

    Update model

--- Models/ViewModels/PubliComment.cs
+++ Models/ViewModels/PubliComment.cs
@@ -6,6 +6,7 @@ namespace FurryFriendFinder.Models.ViewModels
     {
         public List<Comment> Comment = new();
         public List<Publication> publi = new();
+        public int id = new int();
         public PubliComment(List<Publication> publications, List<Comment> comments)
         {
             Comment = comments;
